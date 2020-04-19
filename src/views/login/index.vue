7c107fd3afb10511b017fabb21c3e2533d69385f
YangLong <13422799307@163.com>
2020-04-19 10:51:51 +0800

    黑马头条PC-登录模块-页面布局及样式

---
+++ src/views/login/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:60%" placeholder="验证码"></el-input>
+          <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .login{
+    background-image: url('../../assets/img/back1.jpg');
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+      width: 400px;
+      height: 350px;
+      .title{
+        text-align: center;
+        margin-bottom: 25px;
+        img{
+          height:40px;
+        }
+      }
+    }
+  }
+</style>
