f7b0f0dfa82e0652e3246ceb735bbc97bdbae8c1
YangLong <13422799307@163.com>
2020-04-20 10:09:46 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'// 引入模块
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'// 引入初始化样式
-
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
