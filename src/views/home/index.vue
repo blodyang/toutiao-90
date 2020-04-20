adc77a69ddf78beac514cf908191aa259ee5f467
YangLong <13422799307@163.com>
2020-04-20 11:32:34 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>主页</div>
+<el-container>
+  <el-aside>
+    左侧内容
+  </el-aside>
+  <el-container>
+    <el-header>
+      头部
+    </el-header>
+    <el-main>主要区域</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
