3b70d6bdcb59c59917c588830090f9ae23dbd0d2
YangLong <13422799307@163.com>
2020-05-19 17:28:17 +0800

    主页模块-左侧导航配置路由

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,7 +7,9 @@
     <el-header>
       <layout-header></layout-header>
     </el-header>
-    <el-main>主要区域</el-main>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
   </el-container>
 </el-container>
 </template>
