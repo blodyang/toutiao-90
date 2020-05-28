5fc56f95f75b6547ca9f0bef71e21f7291c765a3
YangLong <13422799307@163.com>
2020-05-28 09:53:06 +0800

    主页模块-导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
+import Home2 from '../views/home/home.vue'
 
 Vue.use(VueRouter)
 
@@ -13,7 +14,11 @@ const routes = [
   {
     path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [{
+      path: '',
+      component: Home2
+    }]
   }, {
     path: '/login',
     component: Login
---
+++ src/views/home/home.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <img src="../../assets/img/img3.jpg" alt="" height="460px">
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
