2abf59603879b0ab2d0dd126cc73e0d0a09b0a98
YangLong <13422799307@163.com>
2020-05-30 22:40:04 +0800

    评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-      评论列表
-  </div>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">
+            评论管理
+        </template>
+      </bread-crumb>
+      <!-- 表格 -->
+      <el-table>
+          <el-table-column width="500" label="标题"></el-table-column>
+          <el-table-column label="评论状态"></el-table-column>
+          <el-table-column label="总评论数"></el-table-column>
+          <el-table-column label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
