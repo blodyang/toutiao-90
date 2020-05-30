fe5ad9185a08b8134ec65bb1b34954f0d3c32d58
YangLong <13422799307@163.com>
2020-05-30 11:00:14 +0800

    接口访问-axios拦截器-统一处理请求token

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -1,76 +1,81 @@
 <template>
-<el-row type="flex" align="middle" class="layout-header">
-    <el-col class="left" :span="12">
-        <i class="el-icon-s-fold"></i>
-        <span>江苏传智播客教育科技股份有限公司</span>
-    </el-col>
-    <el-col :span="12">
-        <el-row class="right" type="flex" justify="end" align="middle">
-            <img :src="!userInfo.photo ? userInfo.photo :defaultImg" alt="">
-            <el-dropdown @command="clickMenu">
-                <span>{{userInfo.name}}</span>
-                <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item command="info">个人信息</el-dropdown-item>
-                    <el-dropdown-item command="git">git地址</el-dropdown-item>
-                    <el-dropdown-item command="lgout">退出登录</el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-        </el-row>
-    </el-col>
-</el-row>
+<!-- el-row -->
+  <el-row class='layout-header' type='flex' align="middle">
+      <!-- 先定义一行 -->
+     <el-col class='left' :span="12">
+         <i class='el-icon-s-fold'></i>
+         <span>江苏传智播客教育科技股份有限公司</span>
+     </el-col>
+     <el-col class='right' :span="12">
+         <el-row type='flex' justify="end" align="middle">
+             <img :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
+             <!-- 下拉菜单 -->
+             <el-dropdown @command="clickMenu">
+                 <!-- 匿名插槽  下拉菜单显示的元素内容 -->
+                 <span>{{ userInfo.name }}</span>
+                 <el-dropdown-menu slot="dropdown">
+                     <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                     <el-dropdown-item command="git">git地址</el-dropdown-item>
+                     <el-dropdown-item command="lgout">退出</el-dropdown-item>
+                 </el-dropdown-menu>
+             </el-dropdown>
+         </el-row>
+     </el-col>
+
+  </el-row>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      userInfo: {},
-      defaultImg: require('../../assets/img/header.jpg')
+      userInfo: {}, // 定义一个用户对象
+      defaultImg: require('../../assets/img/header.jpg') // 先将图片转化成了一个变量
     }
   },
   created () {
-    const token = localStorage.getItem('user-token')
-
     this.$axios({
-      url: '/user/profile',
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+      url: '/user/profile'
     }).then(result => {
       this.userInfo = result.data.data
     })
   },
   methods: {
+    //   点击菜单项时触发
     clickMenu (command) {
       if (command === 'info') {
 
       } else if (command === 'git') {
-        window.location.href = 'https://www.baidu.com'
+        //   跳转到git地址
+        window.location.href = 'https://github.com/shuiruohanyu/90heimatoutiao'
       } else {
-        window.localStorage.removeItem('user-tonken')
-        this.$router.push('/login')
+        //    退出
+        window.localStorage.removeItem('user-token') // 删除令牌
+        this.$router.push('/login') // 回到登录页
       }
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
-.layout-header{
-    .left{
+<style lang='less' scoped>
+.layout-header {
+    height:60px;
+    .left {
         font-size: 20px;
-        span{
-            color:#2c3e50;
-            font-size: 16px;
-            margin-left: 5px;
+        span {
+           color: #2c3e50;
+           font-size: 16px;
+           margin-left:4px;
         }
     }
-    .right{
-        img{
-            width: 40px;
-            border-radius: 50%;
-            margin-right: 5px;
-        }
+    .right {
+      img {
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          margin-right: 5px;
+      }
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import ElementUI from 'element-ui'// 引入模块
 import Commponent from './components'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'// 引入初始化样式
-import axios from 'axios'
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+import axios from './utils/request'
+
 Vue.prototype.$axios = axios
 Vue.use(ElementUI)// 全局注册
 Vue.use(Commponent)// 全局注册
