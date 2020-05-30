96120e557413090462222a8cc8cdda2e36a2b2cb
YangLong <13422799307@163.com>
2020-05-30 19:06:41 +0800

    面包屑组件封装“

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)// 注册全局组件
     Vue.component('layout-header', layoutHeader)// 注册全局组件
+    Vue.component('bread-crumb', breadCrumb)// 全局注册面包屑
   }
 }
