66aef2cf4431ce5c125e14d8cd7ada7adab87593
YangLong <13422799307@163.com>
2020-05-31 19:02:36 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,19 +6,45 @@
         </template>
       </bread-crumb>
       <!-- 表格 -->
-      <el-table>
-          <el-table-column width="500" label="标题"></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
-          <el-table-column label="粉丝评论数"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
+      <el-table :data="list">
+          <el-table-column prop="title" width="500" label="标题"></el-table-column>
+          <el-table-column :formatter="formatterBoolean" prop="comment_status" label="评论状态"></el-table-column>
+          <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+          <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作">
+              <template slot-scope="obj">
+
+                <el-button size="small" type="text">修改</el-button>
+                <el-button size="small" type="text"> {{ obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+            </template>
+          </el-table-column>
       </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: 'articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBoolean (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
